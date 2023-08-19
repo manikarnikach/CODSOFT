6a8c72c0db2a4aece0976eb106dde8f723d2cb17
Manikarnika Chauhan <126712190+manikarnikach@users.noreply.github.com>
2023-08-19 20:00:41 +0530

    Update calculator in c++.cpp

--- calculator in c++.cpp
+++ calculator in c++.cpp
@@ -1,8 +1,11 @@
-#include<iostream.h>  
+#include<iostream>  
 #include<stdio.h>  
 #include<conio.h>  
 #include<math.h>  
 #include<stdlib.h>  
+
+using namespace std;
+
 void add();  
 void sub();  
 void multi();  
@@ -10,9 +13,9 @@ void division();
 void sqr();  
 void srt();  
 void exit();  
-void main()  
+int main()  
 {  
-clrscr();  
+ 
 int opr;  
 // display different operation of the calculator  
 do  
@@ -127,4 +130,3 @@ cin >> num1;
 q = sqrt(num1);  
 cout <<" \n Square Root of " << num1<< " is : "<< q;  
 }  
-
