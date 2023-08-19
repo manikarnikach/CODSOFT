9d0eea01aede56f1a950f9089d7b5070cf549feb
Manikarnika Chauhan <126712190+manikarnikach@users.noreply.github.com>
2023-08-19 20:18:12 +0530

    Update Student Grading System in C++.cpp

--- Student Grading System in C++.cpp
+++ Student Grading System in C++.cpp
@@ -1,35 +1,41 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include<iostream>
+
 using namespace std;
 
 int main()
 {
-    int randomNumber;
-    int guess;
-    int chances = 0;
-    srand(time(0)); 
-    randomNumber = rand() % 100 + 1; 
-    cout << "\t\t\tWelcome to Guess the Number Game\n\n";
+	int english, chemistry, computers, physics, maths; 
+    float total, average, percentage;
 
-    do
-    {
-        cout << "Enter your guess between 1 and 100: ";
-        cin >> guess;
-        chances++;
+    cout << "Please Enter the marks of five subjects: \n";
+    cin >> english >> chemistry >> computers >> physics >> maths;
 
-        if (guess > randomNumber){
-            cout << "The "<<guess<<" is Too high!\n\n";
-        }
-        else if (guess < randomNumber){
-            cout << "The "<<guess<<" is Too low!\n\n";
-        }
-        else{
-            cout << "\nVoila! You got the right guess "<<guess <<"in "<< chances << " tries!\n";
-        }
-    } 
-        while (guess != randomNumber);
+    total = english + chemistry + computers + physics + maths;
+    average = total / 5;
+    percentage = (total / (500)) * 100;
 
-	return 0;
+    cout << "\nTotal Marks      = " << total;
+    cout << "\nAverage Marks    = " << average;
+    cout << "\nMarks Percentage = " << percentage;
+    
+    if(percentage >= 90)    {
+    	cout << "\nGrade A";
+	}
+	else if(percentage >= 80)    {
+    	cout << "\nGrade B";
+	}
+	else if(percentage >= 70)    {
+    	cout << "\nGrade C";
+	}
+	else if(percentage >= 60)    {
+    	cout << "\nGrade D";
+	}
+	else if(percentage >= 40)    {
+    	cout << "\nGrade E";
+	}
+	else {
+    	cout << "\nFail";
+	} 
+	
+ 	return 0;
 }
-
